91d47548924a2e5845353685da8d11a31c132bb3
ricardoferreiracosta08 <ricardo.ferreira.costa08@gmail.com>
2016-04-04 17:00:29 -0300

    Update start.sh

--- 4.0.5/start.sh
+++ 4.0.5/start.sh
@@ -4,6 +4,6 @@ set -e
 wget -q http://downloads.sourceforge.net/project/jboss/JBoss/JBoss-4.0.5.GA/jboss-4.0.5.GA.zip && \
 mv /jboss-4.0.5.GA ${JBOSS_HOME}
 
-chmod +x /*.sh
+chmod +x /*.sh 
 
 exec ${JBOSS_HOME}/bin/run.sh --host=0.0.0.0
